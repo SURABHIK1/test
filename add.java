df5988e7c1a69d0c2aa6bb2b4a05033cd93428ad
SURABHIK1 <161118414+SURABHIK1@users.noreply.github.com>
2025-03-27 12:25:11 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,13 +1,5 @@
 public class add {
     public static void main(String[] args) {
-        // Declare and initialize the numbers
-        int num1 = 10;
-        int num2 = 20;
-
-        // Calculate the sum
-        int sum = num1 + num2;
-
-        // Print the result
-        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("The sum is: " + (10 + 20));
     }
 }
